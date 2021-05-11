1b0f9ff434d1f0eb55a4d38111d6fe146753c80c
Antonio Herazo <antonioherazo7@hotmail.com>
2021-05-10 22:43:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setlongitude] = useState(0);
+  const apiKey = '526969a6e3b91012a7a00d03486a6b25';
+  const url =`api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  const [grados, setGrados] = useState(0);
+  
+  
+  // useEffect(()=>{
+  //   obtenerCoordenadas();
+  // })
+  
+
+  const obtenerDatos = async ()=>{
+    
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function(position){
+        setLatitude(position.coords.latitude);
+        setlongitude(position.coords.longitude);
+      });
+    }
+    const data = await fetch ({url})
+    const coordenadas = data.json()
+    console.log(coordenadas);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <p>latitud: {latitude}</p>
+      <p>longitud: {longitude}</p>
+      <button onClick={()=>{obtenerDatos()}}> click</button>
     </div>
   );
 }
 
-export default App;
+export default App
